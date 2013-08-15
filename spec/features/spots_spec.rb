ef3d770def55885bd79df0ece05d20d9aaf5da26
Sean Doyle <sean.p.doyle24@gmail.com>
2013-08-15 01:37:40 -0400

    continuing to try to TDD

--- spec/features/spots_spec.rb
+++ spec/features/spots_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 feature 'Spots' do
 
-  scenario 'with no spots' do
+  scenario 'viewing no spots' do
     visit spots_path
     expect(page).to have_content /no spots/i
   end
 
-  scenario 'with spots' do
-    spots = create_list(:spot, 2)
+  scenario 'viewing spots' do
+    spots = create_list :spot, 2
 
     visit spots_path
 
@@ -16,6 +16,18 @@ feature 'Spots' do
       within "##{dom_id(spot)}" do
         expect(find('aside')).to have_content spot.name.titleize
       end
+    end
+  end
+
+  scenario 'clicking spot' do
+    spot = create :spot
+
+    visit spots_path
+
+    within "##{dom_id(spot)}" do
+      find("aside").find("a").click
+
+      expect(current_path).to eq spot_path(spot)
     end
   end
 end
