b4dfaaf743a101d9205ae43b86a3df1b58c07d9d
Sean Doyle <sean.p.doyle24@gmail.com>
2013-08-15 20:36:43 -0400

    wrapped paperclip initializer in Config block

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,14 @@
 if Rails.configuration.x.paperclip.s3
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_protocol: "",
-    s3_credentials: {
+  Chief::Application.configure do
+    config.paperclip_defaults = {
+      storage: :s3,
       s3_protocol: "",
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      s3_credentials: {
+        s3_protocol: "",
+        bucket: ENV['AWS_BUCKET'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      }
     }
-  }
+  end
 end
