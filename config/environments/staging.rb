a3f79fc1e54677ceb506bc7dca415c96fe93f5a5
Sean Doyle <sean.p.doyle24@gmail.com>
2013-08-16 17:27:24 -0400

    added configs for elasticache

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,10 +1,9 @@
 Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
 require Rails.root.join('config/initializers/smtp')
 Chief::Application.configure do
-  config.x.paperclip.s3 = true
   config.x.bullet.enabled = true
-  
-  config.cache_store = :dalli_store
+  config.x.cache.elasticache = true
+  config.x.paperclip.s3 = true
   
   # Settings specified here will take precedence over those in config/application.rb.
 
