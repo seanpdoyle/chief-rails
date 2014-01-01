9153f9cc257c2a2a1e6102fbd1d6c2ec856177cc
Sean Doyle <sean.p.doyle24@gmail.com>
2014-01-01 17:47:24 -0500

    spot picture sizes are small -> medium -> large

--- spec/support/matchers/have_picture_for.rb
+++ spec/support/matchers/have_picture_for.rb
@@ -4,7 +4,8 @@ RSpec::Matchers.define :have_picture_for do |record|
     figure = page.find(figure_id)
     expect(figure["title"]).to eq record.name
     expect(figure["data-media"]).to eq record.photo.url(:small)
-    expect(figure["data-media600"]).to eq record.photo.url(:large)
+    expect(figure["data-media480"]).to eq record.photo.url(:medium)
+    expect(figure["data-media800"]).to eq record.photo.url(:large)
 
     noscript = page.find("#{figure_id} noscript img")
     expect(noscript["src"]).to eq record.photo.url(:original)
