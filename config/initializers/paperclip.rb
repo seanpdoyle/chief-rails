ce29c0f6e4096143a68c1f26d055c1ec85c5acc6
Sean Doyle <sean.p.doyle24@gmail.com>
2014-11-21 13:33:06 -0500

    Serve Uploads out of `uploads.wheelbytes.com`

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,7 +3,6 @@ Paperclip.registered_attachments_styles_path = 'config/paperclip_attachments.yml
 if Rails.configuration.x.paperclip.s3
   Paperclip::Attachment.default_options.tap do |options|
     options[:storage] = :s3
-    options[:s3_protocol] = "https"
     options[:s3_headers] = -> attachment do
       {}.tap do |headers|
         if attachment
@@ -11,12 +10,13 @@ if Rails.configuration.x.paperclip.s3
         end
       end
     end
-    options[:url] = ":s3_domain_url"
+    options[:s3_host_alias] = ENV.fetch("UPLOAD_HOST", "uploads.wheelbytes.com")
+    options[:url] = ":s3_alias_url"
     options[:path] = "/:class/:attachment/:id_partition/:style/:filename"
     options[:s3_credentials] = {
-      bucket: ENV["AWS_BUCKET"],
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      bucket: ENV.fetch("AWS_BUCKET"),
+      access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+      secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY")
     }
   end
 end
